313181cd6479018f3fb976eb9e4a8997ec9f680c
Craig Dennis <craig@deliverysoftware.co.uk>
2026-07-30 10:51:56 +0100

    refactor: Remove defaultToday option from BrowseColumnDate make method

--- src/Classes/BrowseColumns/BrowseColumnDate.php
+++ src/Classes/BrowseColumns/BrowseColumnDate.php
@@ -11,15 +11,12 @@ class BrowseColumnDate extends BrowseColumnBase
      *
      * @param ?string  $label  Label of the column
      * @param string  $format  Date format passed to Carbon (defaults to 'd/m/Y')
-     * @param array $options Date specific options: ['defaultToday' => true, 'onEmpty' => '']
+     * @param array $options Date specific options: ['onEmpty' => '']
      */
-    public static function make(?string $label, string $format = 'd/m/Y', array $options = ['defaultToday' => true, 'onEmpty' => '']): static
+    public static function make(?string $label, string $format = 'd/m/Y', array $options = ['onEmpty' => '']): static
     {
         $browseColumnDate = (new static($label))
-            ->setOptions($options)
-            ->when($options['defaultToday'] ?? false, function ($column) {
-                $column->default(Carbon::now()->format('Y-m-d'));
-            });
+            ->setOptions($options);
 
         $browseColumnDate->overrideRenderValue(function ($value) use ($format) {
             if (empty($value)) {
