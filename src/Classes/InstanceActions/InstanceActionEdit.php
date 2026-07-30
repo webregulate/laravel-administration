88e30d5351f42de79edd12f8c99329d05994a810
Craig Dennis <craig@deliverysoftware.co.uk>
2026-07-30 09:51:33 +0100

    feat: Enhance InstanceActionEdit to include browse page condition in edit permission check

--- src/Classes/InstanceActions/InstanceActionEdit.php
+++ src/Classes/InstanceActions/InstanceActionEdit.php
@@ -4,6 +4,7 @@ namespace WebRegulate\LaravelAdministration\Classes\InstanceActions;
 
 use WebRegulate\LaravelAdministration\Classes\InstanceAction;
 use WebRegulate\LaravelAdministration\Classes\ManageableModel;
+use WebRegulate\LaravelAdministration\Classes\WRLAHelper;
 use WebRegulate\LaravelAdministration\Enums\ManageableModelPermissions;
 
 class InstanceActionEdit
@@ -14,7 +15,10 @@ class InstanceActionEdit
         $modelId ??= $manageableModel->model()->id ?? null;
 
         return InstanceAction::make($manageableModel, 'Edit', 'fa fa-edit', 'primary')
-            ->requireCondition($manageableModel::getPermission(ManageableModelPermissions::EDIT))
+            ->requireCondition(
+                $manageableModel::getPermission(ManageableModelPermissions::EDIT)
+                && WRLAHelper::isBrowsePage()
+            )
             ->setAction(route('wrla.manageable-models.edit', [
                 'modelUrlAlias' => $modelUrlAlias,
                 'id' => $modelId,
