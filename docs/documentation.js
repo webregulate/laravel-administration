f1a0e1252b75bdc255c233b65cb20f53a3086b3b
Craig Dennis <craig@deliverysoftware.co.uk>
2026-09-06 15:40:30 +0100

    feat: update version to 0.8.26 and add documentation for alert system and modal improvements

--- docs/documentation.js
+++ docs/documentation.js
@@ -162,7 +162,7 @@ class DocumentationApp {
             { version: '0.8.18', date: '2026-09-03' },
             { version: '0.8.21', date: '2026-09-04' },
             { version: '0.8.24', date: '2026-09-06' },
-            { version: '0.8.25', date: '2026-09-06' },
+            { version: '0.8.26', date: '2026-09-06' },
         ];
     }
 
