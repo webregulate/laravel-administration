a42a58b3d86a40a625e55802418449b85511dc19
Craig Dennis <craig@deliverysoftware.co.uk>
2026-09-07 22:29:06 +0100

    feat: update version to 0.8.31 and add corresponding documentation

--- docs/documentation.js
+++ docs/documentation.js
@@ -164,7 +164,7 @@ class DocumentationApp {
             { version: '0.8.26', date: '2026-09-06' },
             { version: '0.8.28', date: '2026-09-06' },
             { version: '0.8.29', date: '2026-09-07' },
-            { version: '0.8.30', date: '2026-09-07' },
+            { version: '0.8.31', date: '2026-09-07' },
         ];
     }
 
